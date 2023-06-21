37ec101bb082ad217f20a791c396936d31136254
DADOU STEVEN <dgv1pro@gmail.com>
2023-06-21 17:08:49 +0200

    message

--- src/main/java/wcs/spring/repository/SchoolRepository.java
+++ src/main/java/wcs/spring/repository/SchoolRepository.java
@@ -2,10 +2,11 @@ package wcs.spring.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import spring.jpa.Customer;
 import wcs.spring.entity.School;
 
 import java.util.List;
-
+@Repository
 public interface SchoolRepository extends JpaRepository<School, Long> {
 }
