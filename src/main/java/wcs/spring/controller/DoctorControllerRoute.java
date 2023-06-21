d407b1d85dc0a2a47c6e83909c8c54e88a712c0a
DADOU STEVEN <dgv1pro@gmail.com>
2023-06-21 14:03:40 +0200

    message

---
+++ src/main/java/wcs/spring/controller/DoctorControllerRoute.java
@@ -0,0 +1,35 @@
+package wcs.spring.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
+import wcs.spring.model.Doctor;
+
+public class DoctorControllerRoute {
+
+    // méthodes
+
+    /**
+     * Spécification de la méthode doctor avec un paramètre
+     * Specifying the doctor method with a parameter
+     * Angabe der Methode doctor mit einem Parameter
+     * 用一个参数指定医生方法
+     * ドクター・メソッドをパラメータで指定する
+     * @param number numéro d'identification de la page, page identification number, Identifikationsnummer der Seite, 页面标识号, ページ識別番号
+     * @return le lien vers la page d'accueil, link to home page, den Link zur Startseite, 通往主页的链接, ホームページへのリンク
+     */
+    @GetMapping("/doctor/{number}")
+    @ResponseBody
+    public Doctor doctor(@PathVariable int number) {
+
+        if (number == 13) {
+            return new Doctor(13, "Jodie Whittaker");
+        } else if (number >= 1 && number <= 12) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "See other");
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de retrouver l'incarnation {number}");
+        }
+    }
+}
