695816d206ffb2223392563d1cb9c3880e6b6698
DADOU STEVEN <dgv1pro@gmail.com>
2023-06-21 17:05:03 +0200

    message

--- src/main/java/wcs/spring/controller/SchoolController.java
+++ src/main/java/wcs/spring/controller/SchoolController.java
@@ -1,5 +1,6 @@
 package wcs.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,42 +8,45 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import wcs.spring.entity.School;
+import wcs.spring.repository.SchoolRepository;
+
+import java.util.Optional;
 
 @Controller
 public class SchoolController {
 
-    // TODO : get school repository by dependency injection
+    @Autowired
+    private SchoolRepository schoolRepository;
 
     @GetMapping("/schools")
     public String getAll(Model model) {
-
-        // TODO : find all schools
-
+        model.addAttribute("schools", schoolRepository.findAll());
         return "schools";
     }
 
     @GetMapping("/school")
     public String getSchool(Model model,
                             @RequestParam(required = false) Long id) {
-
-        // TODO : find one school by id
-
+        School school= new School();
+        if(id !=null) {
+            Optional<School> optionalSchool= schoolRepository.findById(id);
+            if(optionalSchool.isPresent()) {
+                school=optionalSchool.get();
+            }
+        }
+        model.addAttribute("school", school);
         return "school";
     }
 
     @PostMapping("/school")
     public String postSchool(@ModelAttribute School school) {
-
-        // TODO : create or update a school
-
+        School save = schoolRepository.save(school);
         return "redirect:/schools";
     }
 
     @GetMapping("/school/delete")
     public String deleteSchool(@RequestParam Long id) {
-
-        // TODO : delete a school
-
+        schoolRepository.deleteById(id);
         return "redirect:/schools";
     }
 }
