fc4bee4b53c1a1cda6359a488b88677f14084ffc
DADOU STEVEN <dgv1pro@gmail.com>
2023-06-21 11:13:53 +0200

    message

---
+++ src/main/java/com/example/grangprojetbigproject/GrangProjetBigProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.grangprojetbigproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrangProjetBigProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrangProjetBigProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grangprojetbigproject/GrangProjetBigProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grangprojetbigproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrangProjetBigProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
