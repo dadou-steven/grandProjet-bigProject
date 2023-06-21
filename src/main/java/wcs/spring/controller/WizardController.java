ea55ffc004de0df2f1e63fcbf77a63eff84b4e5f
DADOU STEVEN <dgv1pro@gmail.com>
2023-06-21 17:17:32 +0200

    message

---
+++ src/main/java/wcs/spring/controller/WizardController.java
@@ -0,0 +1,58 @@
+package wcs.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import wcs.spring.entity.Wizard;
+import wcs.spring.repository.WizardRepository;
+
+import java.util.Optional;
+
+@Controller
+public class WizardController {
+
+    @Autowired
+    private WizardRepository repository;
+
+    @GetMapping("/wizards")
+    public String getAll(Model model) {
+
+        model.addAttribute("wizards", repository.findAll());
+
+        return "wizards";
+    }
+
+    @GetMapping("/wizard")
+    public String getWizard(Model model,
+                            @RequestParam(required = false) Long id) {
+
+        Wizard wizard = new Wizard();
+        if (id != null) {
+            Optional<Wizard> optionalWizard = repository.findById(id);
+            if (optionalWizard.isPresent()) {
+                wizard = optionalWizard.get();
+            }
+        }
+        model.addAttribute("wizard", wizard);
+        return "wizard";
+    }
+
+    @PostMapping("/wizard")
+    public String postWizard(@ModelAttribute Wizard wizard) {
+
+        repository.save(wizard);
+        return "redirect:/wizards";
+    }
+
+    @GetMapping("/wizard/delete")
+    public String deleteWizard(@RequestParam Long id) {
+
+        repository.deleteById(id);
+
+        return "redirect:/wizards";
+    }
+}
