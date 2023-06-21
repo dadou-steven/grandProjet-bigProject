9f1ae996fd00794d1579110fe9e7cefd8eb2c5de
DADOU STEVEN <dgv1pro@gmail.com>
2023-06-21 12:45:40 +0200

    message

---
+++ src/main/java/wcs/spring/controller/DoctorController.java
@@ -0,0 +1,41 @@
+package wcs.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DoctorController {
+
+    // méthodes
+
+    /**
+     * Spécification sans paramètre de la méthode william
+     * @return le lien de la page d'accueil
+     */
+    @GetMapping("/doctor/1")
+    @ResponseBody
+    public String william() {
+        return "Docteur <a href='http://localhost:8080/'>William Hartnell</a>";
+    }
+
+    /**
+     * Spécification sans paramètre de la méthode david
+     * @return le lien de la page d'accueil
+     */
+    @GetMapping("/doctor/10")
+    @ResponseBody
+    public String david() {
+        return "Docteur <a href='http://localhost:8080/'>David Tennant</a>";
+    }
+
+    /**
+     * Spécification sans paramètre de la méthode jodie
+     * @return le lien de la page d'accueil
+     */
+    @GetMapping("/doctor/13")
+    @ResponseBody
+    public String jodie() {
+        return "Docteur <a href='http://localhost:8080/'>Jodie Whittaker</a>";
+    }
+}
